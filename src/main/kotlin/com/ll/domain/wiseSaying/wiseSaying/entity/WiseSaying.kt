4e52c4b18fca1be784b2c57589911b6e363e05dd
baekminjin <bhs3392@naver.com>
2025-02-17 12:23:18 +0900

    feat: findById 구현, fromJsonStr 구현

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
@@ -1,28 +1,45 @@
 package com.ll.domain.wiseSaying.wiseSaying.entity
 
-data class WiseSaying ( // data class: 데이터를 담는 클래스
+data class WiseSaying(
     var content: String,
-    var author: String
-) {
+    var author: String,
     var id: Int = 0
-
+) {
     fun modify(content: String, author: String) {
         this.content = content
         this.author = author
     }
 
-    fun isNew(): Boolean { // 새로운 데이터인지 확인
-        return id == 0
-    }
+    fun isNew() = id == 0
 
     val json: String
-        get() {
-            return """
-                {
-                    "id": $id,
-                    "content": "$content",
-                    "author": "$author"
-                }
-            """.trimIndent()
+        get() = """
+            {
+                "id":$id,
+                "content":"$content",
+                "author":"$author"
+            }
+            """
+            .trimIndent()
+
+    companion object {
+        fun fromJson(jsonStr: String): WiseSaying {
+            val map = jsonStrToMap(jsonStr)
+            return WiseSaying(
+                content = map["content"] ?: "",
+                author = map["author"] ?: "",
+                id = (map["id"] ?: "0").toInt()
+            )
         }
+
+        private fun jsonStrToMap(jsonStr: String): Map<String, String> {
+            return jsonStr
+                .removeSurrounding("{", "}")
+                .split(",")
+                .mapNotNull {
+                    val keyValue = it.split(":", limit = 2).map { it.trim().removeSurrounding("\"") }
+                    if (keyValue.size == 2) keyValue[0] to keyValue[1] else null
+                }.toMap()
+        }
+    }
 }
