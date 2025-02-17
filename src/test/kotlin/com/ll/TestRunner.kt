91a47571fef99c740e7d17a4fb720b2ea676e62c
baekminjin <bhs3392@naver.com>
2025-02-17 15:12:10 +0900

    feat: 14 단계 시작, 페이징을 위한 테스트케이스와 샘플 데이터 생성 기능 구현

--- src/test/kotlin/com/ll/TestRunner.kt
+++ src/test/kotlin/com/ll/TestRunner.kt
@@ -1,11 +1,11 @@
 package com.ll
+
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.PrintStream
 
 object TestRunner {
-
     private val originalIn: InputStream = System.`in`
     private val originalOut: PrintStream = System.out
 
@@ -16,16 +16,16 @@ object TestRunner {
 
         return ByteArrayOutputStream().use { outputStream ->
             PrintStream(outputStream).use { printStream ->
-
                 try {
                     System.setIn(
                         ByteArrayInputStream(
                             formattedInput.toByteArray()
                         )
                     )
-                    System.setOut(printStream)
-                    App().run()
 
+                    System.setOut(printStream)
+
+                    App().run()
                 } finally {
                     System.setIn(originalIn)
                     System.setOut(originalOut)
@@ -37,5 +37,14 @@ object TestRunner {
                 .trim()
                 .replace("\r\n", "\n")
         }
+    }
+
+    fun makeSampleData(size: Int) {
+        val input = (1..size)
+            .joinToString("\n") { i ->
+                "등록\n명언 $i\n작자미상\n"
+            }
+
+        run(input)
     }
 }
